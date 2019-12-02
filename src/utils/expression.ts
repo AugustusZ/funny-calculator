8f875cd9ab75c9f3d5d6f6322599cc715a3cfec5
AugustusZ <augustus.z@outlook.com>
2019-12-02 00:45:34 -0800

    Updates

--- src/utils/expression.ts
+++
@@ -1,8 +0,0 @@
-export const checkExpression = (expression: string): number | undefined => {
-  try {
-    // eslint-disable-next-line
-    return Function(`"use strict";return (${expression})`)();
-  } catch {
-    return undefined;
-  }
-};
